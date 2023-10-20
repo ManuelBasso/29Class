4ff13421195f7f7e1e6930a5450c5ad4b3a6987b
ManuelBasso <manuel.basso.89@gmail.com>
2023-10-20 14:38:13 +0200

    Exercise completed

---
+++ _29Class/src/Car.java
@@ -0,0 +1,54 @@
+public class Car {
+    public int  cilindrata;
+    public String  targa;
+    public String  auto;
+    public String  modello;
+
+    public Car (int cilindrata, String targa, String auto, String modello){
+        this.cilindrata = cilindrata;
+        this.targa = targa;
+        this.auto = auto;
+        this.modello = modello;
+    }
+
+    public String getTarga() {
+        return targa;
+    }
+
+    public void setTarga(String targa) {
+        this.targa = targa;
+    }
+
+    public int getCilindrata() {
+        return cilindrata;
+    }
+
+    public void setCilindrata(int cilindrata) {
+        this.cilindrata = cilindrata;
+    }
+
+    public String getAuto() {
+        return auto;
+    }
+
+    public void setAuto(String auto) {
+        this.auto = auto;
+    }
+
+    public String getModello(){
+        return  modello;
+    }
+
+    public void setModello(String modello) {
+        this.modello = modello;
+    }
+
+    public void showDetails(){
+        System.out.println("Auto: " + auto);
+        System.out.println("Modello : " + modello);
+        System.out.println("Cilindrata: " + cilindrata);
+        System.out.println("Targa: " + targa);
+    }
+
+
+}
---
+++ _29Class/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Car firstAuto = new Car(2200,"AV502WW","Fiat", "Panda");
+        firstAuto.showDetails();
+   }
+}
